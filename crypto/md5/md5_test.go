fc4005e0c8ce7a780acfad9e2c8149ca54f5d86c
bronze1man <bronze1man@gmail.com>
2013-10-22 00:31:23 +0800

    save prcess

---
+++ crypto/md5/md5_test.go
@@ -0,0 +1,25 @@
+package md5
+
+import (
+	"testing"
+	"kmg/kmpTest"
+	"encoding/hex"
+	"strings"
+)
+func TestBytes(t *testing.T){
+	expect_bytes,_:=hex.DecodeString("d41d8cd98f00b204e9800998ecf8427e")
+	kmpTest.Assert(t,Bytes([]byte("")),expect_bytes)
+	kmpTest.Assert(t,Bytes([]byte("")),expect_bytes)
+	expect_bytes,_=hex.DecodeString("9e107d9d372bb6826bd81d3542a419d6")
+	kmpTest.Assert(t,Bytes([]byte("The quick brown fox jumps over the lazy dog")),
+		expect_bytes)
+
+}
+func TestHex(t *testing.T){
+	kmpTest.Assert(t,Hex([]byte("")),"d41d8cd98f00b204e9800998ecf8427e")
+	kmpTest.Assert(t,Hex([]byte("The quick brown fox jumps over the lazy dog")),
+	"9e107d9d372bb6826bd81d3542a419d6")
+}
+func TestHexFromString(t *testing.T){
+	kmpTest.Assert(t,HexFromString(strings.Repeat("1",10000)),"b223cca8b360eae4e49568512e2de29f")
+}
---
+++ errors/errors.go
@@ -0,0 +1,29 @@
+package errors
+import "fmt"
+
+import "runtime"
+type MsgWarpError struct{
+	Err error
+	Msg string
+}
+func (err *MsgWarpError)Error()string{
+	return fmt.Sprintf("[%s] %v",err.Msg,err.Err)
+}
+
+//add more message to an error
+func AddMessage(err error,msg string) error{
+	return &MsgWarpError{Err:err,Msg:msg}
+}
+type CallerWarpError struct{
+	Err error
+	File string
+	Line int
+}
+func (err *CallerWarpError)Error()string{
+	return fmt.Sprintf("[%s:%d] %v",err.File,err.Line,err.Err)
+}
+func AddCaller(err error) error{
+	errOut := &CallerWarpError{Err:err}
+	_,errOut.File,errOut.Line,_=runtime.Caller(1)
+	return errOut
+}
