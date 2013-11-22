0b02e649bf44092312f7188d642ad265ec6f0b03
bronze1man <bronze1man@gmail.com>
2013-11-22 11:45:37 +0800

    commit by script

--- kmgSql/GetTableData.go
+++ kmgSql/GetTableData.go
@@ -14,3 +14,24 @@ func (db *Db) MustGetTableData(tableName string) (output []map[string]string) {
 	}
 	return
 }
+
+func (db *Db) GetTableDataMap(tableName string, pkName string) (output map[string]map[string]string, err error) {
+	grid, err := db.GetTableData(tableName)
+	output = make(map[string]map[string]string)
+	for _, row := range grid {
+		pk, ok := row[pkName]
+		if !ok {
+			return nil, fmt.Errorf("GetTableDataMap: pkName colum not exits, tableName: %s, pkName: %s", tableName, pkName)
+		}
+		output[pk] = row
+	}
+	return
+}
+
+func (db *Db) MustGetTableDataMap(tableName string, pkName string) (output map[string]map[string]string) {
+	output, err := db.GetTableDataMap(tableName, pkName)
+	if err != nil {
+		panic(err)
+	}
+	return
+}
