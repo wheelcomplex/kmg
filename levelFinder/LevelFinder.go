8ddc4adbce1d1cc705856c5cb8ee523d59f19136
bronze1man <bronze1man@gmail.com>
2014-01-02 23:12:33 +0800

    commit by script

---
+++ levelFinder/LevelFinder.go
@@ -0,0 +1,65 @@
+package levelFinder
+
+type LevelProvider interface {
+	//根据等级获得升级经验(即累计经验达到这个经验升lv+1级)
+	//当前经验为升级经验时,处于lv+1
+	//默认升到1级,需要0经验(初始1级,0经验)
+	//输入范围 1-MaxLevel()-1 ,两端包含
+	GetExpByLevel(lv int) int
+	//最大等级(到达这个等级以后,不能升级了)
+	MaxLevel() int
+}
+
+type ArrayLevelProvider []int
+
+func (provider ArrayLevelProvider) GetExpByLevel(lv int) int {
+	return provider[lv-1]
+}
+func (provider ArrayLevelProvider) MaxLevel() int {
+	return len(provider) + 1
+}
+
+type LevelExpResult struct {
+	Exp                   int  //规整经验(处理负经验问题和满经验问题)
+	Level                 int  //等级
+	CurrentLevelExcessExp int  // 当前等级溢出经验(满经验时,这个值为0)
+	NextLevelAllNeedExp   int  // 从lv 0 exp升到 lv+1 0exp需要的经验(满经验时,这个值为0)
+	IsMaxLevel            bool //是否满级
+}
+
+//根据经验获取等级,
+//合理处理边界情况,不报错
+//负累计经验重置为0累计经验
+//累计经验超过满级经验重置为 满级经验
+func GetLevelByExp(provider LevelProvider, exp int) (result LevelExpResult) {
+	if exp < 0 {
+		result.Exp = 0
+	} else {
+		result.Exp = exp
+	}
+	//找当前等级
+	maxLevel := provider.MaxLevel()
+	level := maxLevel
+	for i := 1; i <= maxLevel-1; i++ {
+		thisLevelExp := provider.GetExpByLevel(i)
+		if exp >= thisLevelExp {
+			continue
+		}
+		level = i
+		break
+	}
+	result.Level = level
+	if level == maxLevel {
+		result.Exp = provider.GetExpByLevel(maxLevel - 1)
+		result.IsMaxLevel = true
+		return
+	}
+	if level == 1 {
+		result.CurrentLevelExcessExp = result.Exp
+		result.NextLevelAllNeedExp = provider.GetExpByLevel(1)
+		return
+	}
+	result.CurrentLevelExcessExp = result.Exp - provider.GetExpByLevel(level-1)
+	result.NextLevelAllNeedExp = provider.GetExpByLevel(level) - provider.GetExpByLevel(level-1)
+	return
+}
---
+++ levelFinder/LevelFinder_test.go
@@ -0,0 +1,36 @@
+package levelFinder
+
+import (
+	"github.com/bronze1man/kmg/test"
+	"testing"
+)
+
+func TestLevelFinder(ot *testing.T) {
+	t := test.NewTestTools(ot)
+	ExpTable := ArrayLevelProvider{12, 29, 57}
+	levelTestTable := []struct {
+		exp        int
+		actualExp  int
+		lv         int
+		excess     int
+		nextAll    int
+		isMaxLevel bool
+	}{
+		{11, 11, 1, 11, 12, false},
+		{12, 12, 2, 0, 17, false},
+		{13, 13, 2, 1, 17, false},
+		{-1, 0, 1, 0, 12, false},
+		{56, 56, 3, 27, 28, false},
+		{57, 57, 4, 0, 0, true},
+		{58, 57, 4, 0, 0, true},
+		{1000, 57, 4, 0, 0, true},
+	}
+	for _, c := range levelTestTable {
+		result := GetLevelByExp(ExpTable, c.exp)
+		t.EqualMsg(result.Exp, c.actualExp, "actualExp at exp:%d", c.exp)
+		t.EqualMsg(result.Level, c.lv, "Level at exp:%d", c.exp)
+		t.EqualMsg(result.CurrentLevelExcessExp, c.excess, "CurrentLevelExcessExp at exp:%d", c.exp)
+		t.EqualMsg(result.NextLevelAllNeedExp, c.nextAll, "NextLevelAllNeedExp at exp:%d", c.exp)
+		t.EqualMsg(result.IsMaxLevel, c.isMaxLevel, "IsMaxLevel at exp:%d", c.exp)
+	}
+}
