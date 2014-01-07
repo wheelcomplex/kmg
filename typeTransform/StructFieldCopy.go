675d3c1e583d0819ce1354e4451e91b1ab8ff041
bronze1man <bronze1man@gmail.com>
2014-01-07 18:24:00 +0800

    commit by script

--- typeTransform/StructFieldCopy.go
+++ typeTransform/StructFieldCopy.go
@@ -14,6 +14,9 @@ func StructFieldCopy(in interface{}, out interface{}) {
 	tin := vin.Type()
 	vout := reflect.Indirect(reflect.ValueOf(out))
 	tout := vout.Type()
+	if !vout.CanSet() {
+		panic("[StructFieldCopy] out can not set,you have to passing a pointer.")
+	}
 	for i := 0; i < tin.NumField(); i++ {
 		tinf := tin.Field(i)
 		toutf, ok := tout.FieldByName(tinf.Name)
