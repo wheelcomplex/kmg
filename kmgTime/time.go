c20565f3a1a13ab00acabe1640332707bddba6d5
bronze1man <bronze1man@gmail.com>
2014-01-10 20:38:54 +0800

    commit by script

--- kmgTime/time.go
+++ kmgTime/time.go
@@ -6,10 +6,12 @@ import (
 )
 
 const (
-	FormatMysql   = "2006-01-02 15:04:05"
-	Iso3339Hour   = "2006-01-02T15"
-	Iso3339Minute = "2006-01-02T15:04"
-	Iso3339Second = "2006-01-02T15:04:05"
+	FormatMysql     = "2006-01-02 15:04:05"
+	FormatDateMysql = "2006-01-02"
+	Iso3339Hour     = "2006-01-02T15"
+	Iso3339Minute   = "2006-01-02T15:04"
+	Iso3339Second   = "2006-01-02T15:04:05"
+	Day             = 24 * time.Hour
 )
 
 var ParseFormatGuessList = []string{
@@ -18,6 +20,8 @@ var ParseFormatGuessList = []string{
 	Iso3339Minute,
 	Iso3339Second,
 }
+
+var BeijingZone = time.FixedZone("Asia/Beijing", 8*60*60)
 
 func ParseAutoInLocal(sTime string) (t time.Time, err error) {
 	return ParseAutoInLocation(sTime, time.Local)
@@ -54,5 +58,22 @@ func MustFromLocalMysqlFormat(timeString string) time.Time {
 }
 
 func ToLocal(t time.Time) time.Time {
-	return t.In(time.Local)
+	return t.Local()
+}
+
+func ToDateString(t time.Time) string {
+	return t.Format(FormatDateMysql)
+}
+
+func ToDate(t time.Time) time.Time {
+	y, m, d := t.Date()
+	return time.Date(y, m, d, 0, 0, 0, 0, t.Location())
+}
+
+func DateSub(t1 time.Time, t2 time.Time, loc *time.Location) time.Duration {
+	return ToDate(t1.In(loc)).Sub(ToDate(t2.In(loc)))
+}
+
+func DateSubLocal(t1 time.Time, t2 time.Time) time.Duration {
+	return DateSub(t1, t2, time.Local)
 }
