01e6926a6ec624892023dfb5ef0d78559d6c1935
bronze1man <bronze1man@gmail.com>
2014-01-20 11:23:53 +0800

    commit by script

---
+++ kmgTime/Period.go
@@ -0,0 +1,60 @@
+package kmgTime
+
+import (
+	"fmt"
+	"sort"
+	"time"
+)
+
+// a period time start from Start,end to End,
+// start must before or equal end
+type Period struct {
+	Start time.Time
+	End   time.Time
+}
+
+func (p Period) IsIn(t time.Time) bool {
+	if t.After(p.End) {
+		return false
+	}
+	if t.Before(p.Start) {
+		return false
+	}
+	return true
+}
+
+// start must before or equal end
+func NewPeriod(Start time.Time, End time.Time) (period Period, err error) {
+	if Start.After(End) {
+		err = fmt.Errorf("[kmgTime.NewPeriod] Start.After(End) Start:%s End:%s", Start, End)
+		return
+	}
+	return Period{Start: Start, End: End}, nil
+}
+
+//SortedList should sort by start time and should not overlap each other
+func GetPeriodFromSortedList(t time.Time, SortedList []Period) (index int, ok bool) {
+	for index, thisPeriod := range SortedList {
+		if thisPeriod.End.Before(t) && thisPeriod.Start.After(t) {
+			return index, true
+		}
+	}
+	return 0, false
+}
+
+type PeriodSlice []Period
+
+func (p PeriodSlice) Len() int {
+	return len(p)
+}
+
+func (p PeriodSlice) Less(i, j int) bool {
+	return p[i].Start.Before(p[j].Start)
+}
+func (p PeriodSlice) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+}
+
+func PeriodSort(p []Period) {
+	sort.Sort(PeriodSlice(p))
+}
