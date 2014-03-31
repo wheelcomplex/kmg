6f3d984ed071c35730c8f004be162cd11af06181
bronze1man <bronze1man@gmail.com>
2014-03-31 10:21:04 +0800

    修正bug

--- kmgCaptcha/SimpleCaptcha.go
+++ kmgCaptcha/SimpleCaptcha.go
@@ -12,16 +12,18 @@ func SimpleCaptcha(w http.ResponseWriter)(code string,err error){
 	digits:=captcha.RandomDigits(4)
 	image:=captcha.NewImage(digits,240,80)
 	w.Header().Set("Content-Type","image/png")
-	_,err:=image.WriteTo(w)
+	_,err=image.WriteTo(w)
 	if err!=nil{
 		return
 	}
 	//取值范围很小,暂不在此做过多安全性
-	code:=base64.URLEncoding.EncodeToString(sha256.Sum256(digits)[:])
+	codeBytes:= sha256.Sum256(digits)
+	code=base64.URLEncoding.EncodeToString(codeBytes[:])
 	return
 }
 
 func VerifyCode(code string,input string)(ok bool){
-	return base64.URLEncoding.EncodeToString(sha256.Sum256([]byte(input))[:])==code
+	codeBytes:=sha256.Sum256([]byte(input))
+	return base64.URLEncoding.EncodeToString(codeBytes[:])==code
 }
 
